cb88308caaef66a6bec67274bfd39784e70905d1
ram pravesh singh <rampraveshsingh1996@gmail.com>
2024-01-01 22:42:13 +0530

    df

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("https://chat-server-indol-nine.vercel.app");
+const socket = io("https://chat-server-indol-nine.vercel.app/");
 
 const App = () => {
   const [username, setUsername] = useState("");
